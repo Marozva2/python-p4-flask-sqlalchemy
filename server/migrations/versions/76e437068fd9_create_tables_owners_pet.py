30f2d991e182df75faa867f08f44399d2ff62827
Frankline Isaboke <isabokefrankline@gmail.com>
2024-01-16 12:23:10 +0300

    intro to flask_sqlalchemy

---
+++ server/migrations/versions/76e437068fd9_create_tables_owners_pet.py
@@ -0,0 +1,24 @@
+"""Create tables owners, pet
+
+Revision ID: 76e437068fd9
+Revises: 
+Create Date: 2024-01-16 12:18:30.340819
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '76e437068fd9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
